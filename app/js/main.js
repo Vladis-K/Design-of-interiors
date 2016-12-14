339672416406eee289ee7398730213f92a414db4
Vladys <vladis.kh@gmail.com>
2016-12-14 18:35:01 +0200

    AJAX_added

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
-jQuery(function($) {
+jQuery(document).ready(function(){
 
 
-  $(window).scroll(function () {
-    $(".appearance").each(function () {
+
+
+
+  $(window).scroll(function() {
+    $(".appearance").each(function(){
       var positionElements = $(this).offset().top;
       var windowTop = $(window).scrollTop();
       if (positionElements < windowTop + 600) {
@@ -11,9 +14,36 @@ jQuery(function($) {
     });
   });
 
-
-
 });
 
+  var b = document.getElementById("myButton");
+  b.addEventListener ('click', uploadPictures, false);
 
+  function uploadPictures() {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.onreadystatechange = function() {
 
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var getData = allTogether(JSON.parse(xhr.responseText));
+      }
+    };
+
+    xhr.open('GET', 'info.json', true);
+    xhr.send(null);
+
+};
+
+  function allTogether(getData){
+    console.log(getData);
+    var out = "";
+    var allData = getData.response.length;
+    console.log(allData);
+    for (var i = 6; i < allData; i++) {
+      out += '<div class="col-xs-12 col-sm-6 col-md-4">';
+      out += '<img src="' + getData.response[i].image_medium + '"'+'class="img-responsive" alt="img">';
+      out += '<div class="caption">' + '<p>' + getData.response[i].title + '</p>';
+      out += '</div>' + '</div>'
+    }
+    document.getElementById("download").innerHTML = out;
+}
