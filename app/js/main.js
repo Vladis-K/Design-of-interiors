79a8ab1c06fdc049a79d92b9a36fe278580441d5
Vladys <vladis.kh@gmail.com>
2016-12-14 12:57:58 +0200

    Main_changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,8 @@
 jQuery(function($) {
-  "use strict";
 
-  var owlPricing;
-  var ratio = 2;
 
-  $(window).scroll(function() {
-    $(".appearance").each(function(){
+  $(window).scroll(function () {
+    $(".appearance").each(function () {
       var positionElements = $(this).offset().top;
       var windowTop = $(window).scrollTop();
       if (positionElements < windowTop + 600) {
@@ -13,38 +10,10 @@ jQuery(function($) {
       }
     });
   });
-  
-  $(window).load(function() {
-
-    $('.intro-tables, .parallax, header').css('opacity', '0');
-    $('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('.preloader').hide();
-      $('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
-    });
-
-
-    // Sliders Init
-    $('.owl-schedule').owlCarousel({
-      singleItem: true,
-      pagination: true
-    });
-    $('.owl-testimonials').owlCarousel({
-      singleItem: true,
-      pagination: true
-    });
-    $('.owl-twitter').owlCarousel({
-      singleItem: true,
-      pagination: true
-    });
 
 
 
-  
 });
-
-
 
 
 
