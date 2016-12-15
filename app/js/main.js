822a4977fd43951cf43bfc74e564bc9503291321
Vladys <vladis.kh@gmail.com>
2016-12-15 16:59:51 +0200

    Without_content

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,12 @@
 jQuery(document).ready(function(){
 
 
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
 
 
-
+  //Scroll appearance
   $(window).scroll(function() {
     $(".appearance").each(function(){
       var positionElements = $(this).offset().top;
@@ -22,28 +25,29 @@ jQuery(document).ready(function(){
   function uploadPictures() {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
+
     xhr.onreadystatechange = function() {
 
       if (xhr.readyState == 4 && xhr.status == 200) {
-        var getData = allTogether(JSON.parse(xhr.responseText));
+         allTogether(JSON.parse(xhr.responseText));
       }
     };
 
-    xhr.open('GET', 'info.json', true);
+    xhr.open('GET', 'img/info.json', true);
     xhr.send(null);
 
-};
 
   function allTogether(getData){
-    console.log(getData);
-    var out = "";
-    var allData = getData.response.length;
-    console.log(allData);
-    for (var i = 6; i < allData; i++) {
-      out += '<div class="col-xs-12 col-sm-6 col-md-4">';
-      out += '<img src="' + getData.response[i].image_medium + '"'+'class="img-responsive" alt="img">';
-      out += '<div class="caption">' + '<p>' + getData.response[i].title + '</p>';
-      out += '</div>' + '</div>'
+      console.log(getData);
+      var out = "";
+      var allData = getData.response.length;
+      console.log(allData);
+      for (var i = 6; i < allData; i++) {
+          out += '<div class="col-xs-12 col-sm-6 col-md-4">';
+          out += '<img src="' + getData.response[i].image_medium + '"' + ' class="img-responsive" alt="'+ getData.response[i].title +' ">';
+          out += '<div class="depiction">' + '<p>' + getData.response[i].description + ', '+ getData.response[i].title + '</p>';
+          out += '</div>' + '</div>'
+      }
+      document.getElementById("download").innerHTML = out;
     }
-    document.getElementById("download").innerHTML = out;
-}
+  };
