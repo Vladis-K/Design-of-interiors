f15c11235fc172c34283d221173f6d537794885c
Vladys <vladis.kh@gmail.com>
2016-12-20 15:03:36 +0200

    global_changes_and_improvements

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function() {
 
-    // $("#modal_btn").click(function(){
-    //   $("#modalBasic").modal("toggle");
-    // });
-
     //Scroll appearance
     $(window).scroll(function () {
         $(".appearance").each(function () {
@@ -16,10 +12,6 @@ jQuery(document).ready(function() {
     });
 
     //Smooth scrolling
-
-    var menu = document.getElementById("menu");
-
-
     $("#menu").on("click", "a", function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
@@ -29,56 +21,45 @@ jQuery(document).ready(function() {
         }, 2000);
     });
 
-})
+    //Modal switches
+    $("#modalBasic").on('show.bs.modal', function(event){
+        var button = $(event.relatedTarget);
+        var typePackage = button.data('info');
+        var price = button.data('price');
+        $(this).find('.modal-title').text(typePackage + " package");
+        $(this).find('.modal-price').text(price);
+    })
 
 
-// var a = document.getElementById("modal_btn");
-// a.addEventListener('click', uploadModal, false);
-//
-// function uploadModal() {
-//
-//     let xhr = new XMLHttpRequest();
-//     console.log(xhr.status)
-//
-//     xhr.onreadystatechange = function() {
-//         if(xhr.readyState == 4 && xhr.status == 200) {
-//             console.log(xhr.status + "+" + xhr.statusText)
-//
-//             document.getElementById('content').innerHTML = xhr.responseText;
-//         }
-//     };
-//     xhr.open('GET', 'modal.html', true);
-//     xhr.send();
-//
-// }
+});
+    //JavaScript
 
-
-    // setTimeout(gogo, 1200);
-
-    // function gogo(){
+        //Loading additional 3d-visualization
         var b = document.getElementById("myButton");
         b.addEventListener('click', uploadPictures, false);
 
         function uploadPictures() {
             var xhr = new XMLHttpRequest();
-            xhr.withCredentials = true;
+            //xhr.withCredentials = true;
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     allTogether(JSON.parse(xhr.responseText));
                 }
             };
-
             xhr.open('GET', 'img/info.json', true);
             xhr.send(null);
-
+            
             function allTogether(getData) {
                 var out = "";
                 var allData = getData.response.length;
-                console.log(allData);
+                var insert = document.getElementById("upload");
+                //console.log(allData);
                 // var index = 0;
-                top: for (var i = 0; i < allData; i++) {
+                var start = 6;
+               // top:
+                    for (var i = start; i < allData; i++) {
                     getData.response[i];
-                    console.log(getData.response[i]);
+                    //console.log(getData.response[i]);
                     // if (index % 4 == 0){
                     //     break top;
                     // }
@@ -86,13 +67,12 @@ jQuery(document).ready(function() {
                     out += '<img src="' + getData.response[i].image_medium + '"' + ' class="img-responsive" alt=" ' + getData.response[i].title + ' ">';
                     out += '<div class="depiction">' + '<p>' + getData.response[i].description + ', ' + getData.response[i].title + '</p>';
                     out += '</div>' + '</div>'
-
                 }
-
-                document.getElementById("download").innerHTML = out;
+                insert.innerHTML = out;
+                insert.classList.add("slide");
                 b.style.visibility = "hidden";
 
             }
-        }
-    // };
+        };
+
 
